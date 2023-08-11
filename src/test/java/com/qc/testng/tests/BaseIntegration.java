5602f33508c9ee7f0c342c7f42000aeaf497d1b5
test2 <ravichuttar@gmail.com>
2023-08-11 20:25:25 +0530

    Reading data from excel & generated extent report

--- src/test/java/com/qc/testng/tests/BaseIntegration.java
+++ src/test/java/com/qc/testng/tests/BaseIntegration.java
@@ -8,16 +8,19 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.AfterSuite;
 import org.testng.annotations.BeforeSuite;
+import org.testng.annotations.DataProvider;
 
 import com.qc.testng.utils.TestUtils;
 
 public class BaseIntegration {
 	WebDriver driver;
-	WebElement email,pass,submit;
+	WebElement email,pass,submit,logout;
 	Properties prop;
+	String testName;
 	TestUtils test = new TestUtils(); 
 	@BeforeSuite
 	public void setUp() throws IOException { 
+	
 		prop = test.readProp();
 		if(prop.getProperty("browser").equals("chrome")) {
 			System.setProperty(prop.getProperty("chromeKey"),prop.getProperty("chromeVal"));
@@ -27,6 +30,11 @@ public class BaseIntegration {
 		}
 		
 		driver.get(prop.getProperty("siteUrl"));
+		
+	}
+	@DataProvider
+	public Object[][] loginData() throws IOException {
+		return test.readExcelData("logindata");
 		
 	}
 	@AfterSuite
--- src/test/java/com/qc/testng/tests/LoginTest.java
+++ src/test/java/com/qc/testng/tests/LoginTest.java
@@ -10,19 +10,30 @@ public class LoginTest extends BaseIntegration{
 	@BeforeMethod
 	public void locateElement() {
 		email = driver.findElement(By.id("email"));
+		email.clear();
 		pass = driver.findElement(By.id("password"));
+		pass.clear();
 		submit = driver.findElement(By.id("submit"));
 	}
-	  @Test
-	  public void doLogin() {
-		  email.sendKeys("queuecodes@gmail.com");
-		  pass.sendKeys("123456"); 
+	  @Test(dataProvider  = "loginData")
+	  public void doLogin(String tName, String uName, String uPassword) {
+		  testName = tName;
+		  email.sendKeys(uName);
+		  pass.sendKeys(uPassword); 
 		  submit.click();
 	  }
 	  @AfterMethod
-	  public void doAssert() {
+	  public void doAssert() throws InterruptedException {
 		  String actResult = driver.getTitle();
-		  Assert.assertEquals(actResult,"Queue Codes | Dashboard"); 
+		  if(testName.equals("Both are valid")) {
+		Assert.assertEquals(actResult,"Queue Codes | Dashboard");
+		logout = driver.findElement(By.id("hlogout"));
+		logout.click();
+		  }
+		  else {
+		  Assert.assertEquals(actResult,"Queue Codes | Log in"); 
+		  }
+		  Thread.sleep(2000);
 	  }
 	
 
